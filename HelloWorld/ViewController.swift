6a424c3d1ed7e69a609736ec04e97ff350a0a04f
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-07-29 18:04:36 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ao Nagasawa on 2020/07/29.
+//  Copyright © 2020 Ao.Nagasawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
